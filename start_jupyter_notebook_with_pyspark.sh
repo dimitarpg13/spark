7ace02bb2e9a177013d3a56ff2cc3bea046f60bb
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2023-12-13 04:21:42 -0500

    update to spark v3.4.0 and GraphFrames v0.8.4

--- start_jupyter_notebook_with_pyspark.sh
+++ start_jupyter_notebook_with_pyspark.sh
@@ -6,4 +6,4 @@
  export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
  export PYSPARK_DRIVER_PYTHON='jupyter'
  export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --port=8889'
- pyspark --packages graphframes:graphframes:0.8.2-spark3.2-s_2.12
+ pyspark --packages graphframes:graphframes:0.8.3-spark3.4-s_2.13
